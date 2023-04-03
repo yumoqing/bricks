da2054c60998cacfdadcd8e3d99bdf042d2f1a13
yumoqing <yumoqing@gmail.com>
2023-04-03 15:03:37 +0800

    bugfix

---
+++ bricks/install.bat
@@ -0,0 +1,10 @@
+echo copy ..\dist\*.* e:\gadget\wwwroot\bricks
+copy ..\dist\*.* e:\gadget\wwwroot\bricks
+echo copy css\*.* e:\gadget\wwwroot\bricks\css
+copy css\*.* e:\gadget\wwwroot\bricks\css
+copy imgs\*.* e:\gadget\wwwroot\bricks\imgs
+echo copy imgs\*.* e:\gadget\wwwroot\bricks\imgs
+copy ..\docs\*.* e:\gadget\wwwroot\docs
+echo copy ..\docs\*.* e:\gadget\wwwroot\docs
+copy ..\examples\*.* e:\gadget\wwwroot\examples
+echo copy ..\examples\*.* e:\gadget\wwwroot\examples
