95aa7e3f148c0403a8f69f6609006b0c709f7bde
yumoqing <yumoqing@gmail.com>
2023-09-09 17:34:55 +0800

    bugfix

--- bricks/wterm.js
+++ bricks/wterm.js
@@ -19,6 +19,11 @@ class Wterm extends JsWidget {
 		this.term = term;
 		term.open(this.dom_element);
 		var ws = new WebSocket(this.opts.ws_url);
+		console.log('FitAddon=', FitAddon);
+		this.fitAddon = new FitAddon.FitAddon()
+		term.loadAddon(this.fitAddon)
+		// this.fitAddon.fit()
+		this.bind('resize', this.term_resize.bind(this))
 		ws.onmessage = msg => {
 			term.write(JSON.parse(msg.data).data);
 		};
@@ -31,6 +36,15 @@ class Wterm extends JsWidget {
 			}
 			ws.send(key);
 		});
+		term.focus();
+		term.paste("ls -l\n");
+	}
+	term_resize(){
+		try {
+			this.fitAddon.fit();
+		} catch(e){
+			console.log('resize error', e);
+		}
 	}
 }
 
